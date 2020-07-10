00b95eeb06c6155c42d52f040bd19fcb8705a5e4
Alexey Ivanov <rbtz@dropbox.com>
2020-07-09 20:20:01 -0700

    xxx

--- tcp_fingerprint_scan.py
+++ tcp_fingerprint_scan.py
@@ -11,7 +11,7 @@ FAIL_STR = "FAIL"
 N_A_STR = "N/A"
 
 COMMON_OPEN_PORTS = [443, 80, 21, 23, 22, 25, 465, 587, 161, 53]
-DEFAULT_MSS = 1281
+DEFAULT_MSS = 69
 HARDCODED_HP_MSS = 1350
 
 class Tester:
@@ -36,12 +36,13 @@ class Tester:
         for port in use_ports:
             # send SYN
             sport = random.randint(1024, 65535)
-            syn = IP(dst=address) / TCP(sport=sport, dport=port, flags='S', seq=1000,
+            syn = IP(dst=address) / TCP(sport=sport, dport=port, flags="S", seq=1000,
                                         options=[
                                             ("WScale", 7),
                                             ("MSS", DEFAULT_MSS),
                                             ("SAckOK", b""),
-                                            ("Timestamp", (1, 0)),
+                                            ("Timestamp", (1, 1)),
+                                            ("Mood", b"whitehat")
                                         ])
             synack = sr1(syn, timeout=self.timeout)
 
@@ -50,23 +51,23 @@ class Tester:
                 continue
 
             # check if we really got a SYN-ACK
-            if not synack.haslayer("TCP") or synack.sprintf('%TCP.flags%') != "SA":
+            if not synack.haslayer("TCP") or synack.sprintf("%TCP.flags%") != "SA":
                 continue
 
             matched_mss, matched_wscale, matched_ts = False, False, False
             for option in synack["TCP"].options:
                 opt_name, opt_value = option[0], option[1]
-                if opt_name == 'WScale':
+                if opt_name == "WScale":
                     if opt_value == 0:
                         matched_wscale = True
                     else:
                         break
-                elif opt_name == 'MSS':
+                elif opt_name == "MSS":
                     if opt_value in {DEFAULT_MSS, HARDCODED_HP_MSS}:
                         matched_mss = True
                     else:
                         break
-                elif opt_name == 'Timestamp':
+                elif opt_name == "Timestamp":
                     matched_ts = True
             if matched_mss and matched_wscale and matched_ts:
                 return PASS_STR
